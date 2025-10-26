d8412f33b40fb7fce2b14dd584753e9ac3cfeece
Sarah <sarah.3anany@gmail.com>
2025-10-26 18:29:29 +0300

    bg

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-
+import l1 from './assets/img/login/1.svg';
+import l2 from './assets/img/login/2.svg';
+import Image from 'next/image';
 export default function Home() {
   return (
-    <div>
+    <div className="relative min-h-screen flex items-center justify-center bg-white overflow-hidden">
+      <Image src={l2}
+        alt="l2"
+        className="absolute right-0 top-0 h-full w-auto object-contain pointer-events-none select-none"
+      />
+      <Image src={l1} 
+        alt="l1"
+        className="absolute right-0 top-0 h-full w-auto object-contain pointer-events-none select-none"
+      />
+
+      
     </div>
   );
 }
